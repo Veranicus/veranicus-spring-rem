add1dbb33383b98ce16a685bd0f03110ffab3ac0
Veranicus <polacek.patrik@gmail.com>
2021-10-01 12:59:22 +0200

    init commit

---
+++ src/main/java/com/example/patrik/PatrikApplication.java
@@ -0,0 +1,13 @@
+package com.example.patrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/patrik/PatrikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
